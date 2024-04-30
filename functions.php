497934dda2b9da1fe5027246439848e0cfb11709
allysonbelo <allysonbelo@gmail.com>
2024-04-30 13:36:30 -0300

    Add: Header done

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function pds_load_scripts()
 {
     wp_enqueue_style('pds-style', get_stylesheet_uri(), array(), '1.0', 'all');
+    wp_enqueue_style('all-styles', get_theme_file_uri('/styles/all-styles.css'));
 }
 add_action('wp_enqueue_scripts', 'pds_load_scripts');
 
@@ -16,7 +17,25 @@ function add_google_analytics_code()
         gtag('js', new Date());
         gtag('config', 'G-HJY2MRERVP');
     </script> -->
-    <?php
+<?php
 }
 
 add_action('wp_head', 'add_google_analytics_code');
+
+function pds_config()
+{
+    register_nav_menus(
+        array(
+            'pds_header_menu' => 'Header Menu',
+            'pds_footer_menu' => 'Footer Menu'
+        )
+    );
+
+    add_theme_support('custom-logo', array(
+        'height' => 38,
+        'width' => 210,
+        'flex-heigth' => true, 
+        'flex-width' => true
+    ));
+}
+add_action('after_setup_theme', 'pds_config');
--- header.php
+++ header.php
@@ -50,8 +50,79 @@
 
 <body <?php body_class(); ?>>
 
-    <header>
-        Header 
+    <header class="header_mobile">
+        <div class="wrapper">
+            <img class="menu_hamburguer" src="<?php echo get_theme_file_uri('/img/menu-hamburguer.png') ?>" alt="Icone do menu mobile">
+            <div class="side_menu_mobile">
+                <div class="logo_mobile">
+                    <a href="<?php echo home_url('/') ?>" aria-label="Logo plano de saúde">
+                        <img src="<?php echo get_theme_file_uri('/img/logo_planodesaude.png') ?>" alt="">
+                    </a>
+                </div>
+                <nav>
+                    <?php wp_nav_menu(array('theme_location' => 'pds_header_menu', 'depth' => 2, 'menu_class' => 'pds_header_menu')); ?>
+                </nav>
+                <div class="header_search_mobile">
+                    <?php get_search_form(); ?>
+                </div>
+                <img class="close_menu" src="<?php echo get_theme_file_uri('/img/close-icon.png') ?>" alt="Fechar menu">
+            </div>
+            <div class="logo_mobile">
+                <a href="<?php echo home_url('/') ?>" aria-label="Logo plano de saúde">
+                    <img src="<?php echo get_theme_file_uri('/img/logo_planodesaude_mobile.png') ?>" alt="">
+                </a>
+            </div>
+        </div>
     </header>
+
+    <div class="wrapper">
+
+        <header class="header_desktop">
+            <div>
+                <?php if (has_custom_logo()) {
+                    the_custom_logo();
+                } else {
+                ?>
+                    <a href="<?php echo home_url('/') ?>" aria-label="Logo plano de saúde"><span><?php bloginfo('name'); ?></span></a>
+                <?php
+                } ?>
+            </div>
+            <nav>
+                <?php wp_nav_menu(array('theme_location' => 'pds_header_menu', 'depth' => 2, 'menu_class' => 'pds_header_menu')); ?>
+            </nav>
+            <div class="header_search">
+                <?php get_search_form(); ?>
+            </div>
+        </header>
+    </div>
+
+    <script>
+        let hamburguer = document.querySelector('.menu_hamburguer');
+        hamburguer.addEventListener("click", () => {
+            let header_mobile = document.querySelector('.header_mobile');
+            header_mobile.classList.add('active');
+
+            let close_menu = document.querySelector('.close_menu');
+            close_menu.addEventListener('click', () => {
+                header_mobile.classList.remove('active');
+            })
+        })
+
+        document.querySelectorAll('.menu-item-has-children').forEach(item => {
+            item.addEventListener('click', function() {
+
+                let subMenu = this.querySelector('.sub-menu');
+                subMenu.classList.toggle('show');
+
+                if (subMenu.classList.contains('show')) {
+                    this.style.color = '#5B8FCB';
+                    let link = document.appendChild;
+                    console.log(link);
+                } else {
+                    this.style.color = '';
+                }
+            });
+        });
+    </script>
 
     <main>
