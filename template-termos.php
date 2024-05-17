189f2ab3d2c5d538368342b156a0288a9cb63e23
allysonbelo <allysonbelo@gmail.com>
2024-05-17 15:57:34 -0300

    Add: page.php finished

--- template-termos.php
+++ template-termos.php
@@ -1,8 +1,12 @@
 <?php
 
+/*
+Template Name: Pivacidade/Termos
+*/
+
 $custom_link = get_theme_mod('custom_link', '');
 get_header();
-get_template_part('/parts/main-banner');
+get_template_part('/parts/single-banner');
 ?>
 
 <div class="wrapper">
@@ -10,14 +14,13 @@ get_template_part('/parts/main-banner');
         <?php
         if (have_posts()) :  while (have_posts()) : the_post(); ?>
                 <h2 class="page_title"><?php the_title() ?></h2>
-                <div class="page_content">
-                    <?php the_content() ?>
-                </div>
+                <?php the_content() ?>
             <?php endwhile;
         else : ?>
             <p><?php esc_html_e('Sorry, no posts matched your criteria.') ?></p>
         <?php endif; ?>
     </div>
+    <?php get_template_part('/parts/horizontal-cta') ?>
 </div>
 
 
